b67ba6ddb38e542cd93afc2fe12343ded2b0fcd8
emoraisdev <evertonsmorais@gmail.com>
2025-05-24 12:09:03 -0300

    Criando API de Customer

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,7 +1,13 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.Customer
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,7 +15,13 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController {
 
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello World!"
+    fun listar(): Customer {
+        return Customer("1", "Gustavo", "email@email.com")
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun salvar(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
     }
 }
